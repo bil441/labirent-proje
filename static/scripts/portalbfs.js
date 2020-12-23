ebf1ebcf19f1238bdacd86a4b952bb217866ad50
Deniz Türkmen <59728541+mdenizturkmen@users.noreply.github.com>
2020-12-23 16:40:58 +0300

    reformat

--- static/scripts/portalbfs.js
+++ static/scripts/portalbfs.js
@@ -1,4 +1,4 @@
-class Bfs{
+class Bfs {
 
     constructor(strgrid) {
 
@@ -7,46 +7,36 @@ class Bfs{
         var portalstart, portalend;
         var trgtlist = [];
 
-        var cnt=0;
-        var portalflag=false;
+        var cnt = 0;
+        var portalflag = false;
 
-        for(var x = 0, xl = strgrid.length; x < xl; x++) {
+        for (var x = 0, xl = strgrid.length; x < xl; x++) {
             this.grid[x] = [];
-            for(var y = 0, yl = strgrid[0].length; y < yl; y++) {
+            for (var y = 0, yl = strgrid[0].length; y < yl; y++) {
 
-                if(strgrid[x][y]=='wall')
-                    this.grid[x][y]= new Cell(x,y,true, false);
+                if (strgrid[x][y] == 'wall')
+                    this.grid[x][y] = new Cell(x, y, true, false);
 
-                else if(strgrid[x][y]=='empty'){
-                    this.grid[x][y]= new Cell(x,y,false, false);
+                else if (strgrid[x][y] == 'empty') {
+                    this.grid[x][y] = new Cell(x, y, false, false);
                     cnt++;
-                }
+                } else if (strgrid[x][y] == 'portalstart') {
+                    this.grid[x][y] = new Cell(x, y, false, false);
+                    portalstart = this.grid[x][y];
+                    portalflag = true;
+                } else if (strgrid[x][y] == 'portalend') {
+                    this.grid[x][y] = new Cell(x, y, false, false);
+                    portalend = this.grid[x][y];
+                } else if (strgrid[x][y] == 'start') {
+                    this.grid[x][y] = new Cell(x, y, false, false);
+                    this.start = this.grid[x][y];
+                } else if (strgrid[x][y] == 'lava')
+                    this.grid[x][y] = new Cell(x, y, false, true);
 
-                else if(strgrid[x][y]=='portalstart'){
-                    this.grid[x][y]= new Cell(x,y,false, false);
-                    portalstart=this.grid[x][y];
-                    portalflag=true;
-                }
-
-                else if(strgrid[x][y]=='portalend'){
-                    this.grid[x][y]= new Cell(x,y,false, false);
-                    portalend=this.grid[x][y];
-                }
-
-                else if(strgrid[x][y]=='start'){
-                    this.grid[x][y]= new Cell(x,y,false, false);
-                    this.start=this.grid[x][y];
-                }
-
-                else if(strgrid[x][y]=='lava')
-                    this.grid[x][y]= new Cell(x,y,false, true);
-
-                else if(strgrid[x][y]=='goal'){
-                    this.grid[x][y]= new Cell(x,y,false, false);
+                else if (strgrid[x][y] == 'goal') {
+                    this.grid[x][y] = new Cell(x, y, false, false);
                     trgtlist.push(this.grid[x][y]);
                 }
-
-
 
 
             }
@@ -55,7 +45,7 @@ class Bfs{
         this.targets = trgtlist;
 
         if (!portalflag)
-            this.portal=null;
+            this.portal = null;
         else
             this.portal = new Portal(portalstart, portalend);
 
@@ -73,10 +63,10 @@ class Bfs{
         var tmplength;
         //console.log(frontier.printPQueue());
 
-        var steps=0;
+        var steps = 0;
 
         //while(false) {
-        while(frontier.length!=0) {
+        while (frontier.length != 0) {
 
             //console.log("Hello1");
             // Grab the lowest f(x) to process next.  Heap keeps this sorted for us.
@@ -86,14 +76,14 @@ class Bfs{
 
 
             // target case -- result has been found, return the traced path.
-            if(this.isTarget(currentNode)) {
+            if (this.isTarget(currentNode)) {
 
                 console.log("cost:" + currentNode.cost);
-                var direction=[]
+                var direction = []
 
                 var curr = currentNode;
                 var ret = [];
-                while(curr.parent) {
+                while (curr.parent) {
                     ret.push(curr);
                     direction.push(this.getDirection(curr.parent, curr))
                     curr = curr.parent;
@@ -112,17 +102,15 @@ class Bfs{
             // Normal case -- move currentNode from open to closed, process each of its neighbors.
 
 
-
-
             // Find all neighbors for the current node.
             var neighbors = this.neighbors(currentNode);
 
-            for(var i=0, il = neighbors.length; i < il; i++) {
+            for (var i = 0, il = neighbors.length; i < il; i++) {
                 var neighbor = neighbors[i];
 
                 //console.log("("+ neighbor.x + "," + neighbor.y + ")\n");
 
-                if(neighbor.visited || neighbor.isWall || neighbor.isLava) {
+                if (neighbor.visited || neighbor.isWall || neighbor.isLava) {
                     continue;
                 }
 
@@ -136,7 +124,7 @@ class Bfs{
 
             }
 
-            if(this.isPortal(currentNode)){
+            if (this.isPortal(currentNode)) {
                 var portalend = this.grid[this.portal.end.x][this.portal.end.y];
                 portalend.visited = true;
                 portalend.parent = currentNode;
@@ -145,10 +133,10 @@ class Bfs{
             }
 
 
-            tmplength=frontier.length;
+            tmplength = frontier.length;
 
-            if(tmplength>qlength)
-                qlength=tmplength;
+            if (tmplength > qlength)
+                qlength = tmplength;
             //console.log(frontier.printPQueue());
 
             //console.log("Hello3");
@@ -160,7 +148,6 @@ class Bfs{
         console.log("no solution");
         return null;
     };
-
 
 
     neighbors(node) {
@@ -176,88 +163,86 @@ class Bfs{
         //console.log("height: " + height);
 
         // West
-        if(this.grid[x-1] && this.grid[x-1][y]) {
-            ret.push(this.grid[x-1][y]);
+        if (this.grid[x - 1] && this.grid[x - 1][y]) {
+            ret.push(this.grid[x - 1][y]);
         }
 
         // East
-        if(this.grid[x+1] && this.grid[x+1][y]) {
-            ret.push(this.grid[x+1][y]);
+        if (this.grid[x + 1] && this.grid[x + 1][y]) {
+            ret.push(this.grid[x + 1][y]);
         }
 
         // South
-        if(this.grid[x] && this.grid[x][y-1]) {
-            ret.push(this.grid[x][y-1]);
+        if (this.grid[x] && this.grid[x][y - 1]) {
+            ret.push(this.grid[x][y - 1]);
         }
 
         // North
-        if(this.grid[x] && this.grid[x][y+1]) {
-            ret.push(this.grid[x][y+1]);
+        if (this.grid[x] && this.grid[x][y + 1]) {
+            ret.push(this.grid[x][y + 1]);
         }
-
 
 
         return ret;
     };
 
-    isTarget(cell){
+    isTarget(cell) {
 
         //console.log("cell: ("+ cell.x + "," + cell.y + ")\n");
 
 
-        for(var i=0, il=this.targets.length; i<il; i++){
+        for (var i = 0, il = this.targets.length; i < il; i++) {
             //console.log("("+ this.targets[i].x + "," + this.targets[i].y + ")\n");
-            if(this.targets[i].x==cell.x && this.targets[i].y==cell.y)
+            if (this.targets[i].x == cell.x && this.targets[i].y == cell.y)
                 return true;
         }
 
         return false;
     };
 
-    isPortal(cell){
+    isPortal(cell) {
 
-        if(this.portal==null)
+        if (this.portal == null)
             return false;
 
-        if(this.portal.start.x==cell.x && this.portal.start.y==cell.y)
+        if (this.portal.start.x == cell.x && this.portal.start.y == cell.y)
             return true;
 
         return false;
     }
 
-    getDirection(node1, node2){
+    getDirection(node1, node2) {
 
-        var x= node1.x - node2.x;
-        var y= node1.y - node2.y;
+        var x = node1.x - node2.x;
+        var y = node1.y - node2.y;
 
-        if(x==0 && y==1)
+        if (x == 0 && y == 1)
             return 'north'
 
-        if(x==0 && y==-1)
+        if (x == 0 && y == -1)
             return 'south'
 
-        if(x==1 && y==0)
+        if (x == 1 && y == 0)
             return 'west'
 
-        if(x==-1 && y==0)
+        if (x == -1 && y == 0)
             return 'east'
 
         else
-            return  "teleport" + "-" + this.portal.end.x + "-" + this.portal.end.y;
+            return "teleport" + "-" + this.portal.end.x + "-" + this.portal.end.y;
     }
-
 
 
 }
 
-class Cell{
+class Cell {
 
     constructor(xaxis, yaxis, wall, lava) {
-        this.x=xaxis;
-        this.y=yaxis;
-        this.isWall=wall;
-        this.cost=0;
-        this.visited=0;
+        this.x = xaxis;
+        this.y = yaxis;
+        this.isWall = wall;
+        this.cost = 0;
+        this.visited = 0;
         this.parent = null;
         this.isLava = lava
 
